ab451b50d9fe2c85a7d698305b9f58f1e0828d79
Desmond Chin <desmond@desmondchin.xyz>
2019-07-11 16:36:35 +0800

    remove redundant return

--- src/VIEW.php
+++ src/VIEW.php
@@ -45,10 +45,9 @@ class VIEW {
         return $view_file;
     }
 
-    public function twig($data = null){
+    public function renderTwig($data = null){
         $this->_twig = true;
         $view_file = $this->render();
-        return $view_file;
         $loader = new \Twig\Loader\ArrayLoader([
             'twig' => $view_file,
         ]);
