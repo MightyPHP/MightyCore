d491c27efcdb89d2f2b2ca55d255b92ecc538e6c
Desmond Chin <desmond.chin.cc93@gmail.com>
2019-10-16 23:20:58 +0800

    progress on seeding

--- src/CONSOLE.php
+++ src/CONSOLE.php
@@ -16,10 +16,59 @@ class CONSOLE {
         $method = $arg[0];
         $this->$method();
     }
+
+    public function crypto_rand_secure($min, $max)
+    {
+        $range = $max - $min;
+        if ($range < 1) return $min; // not so random...
+        $log = ceil(log($range, 2));
+        $bytes = (int) ($log / 8) + 1; // length in bytes
+        $bits = (int) $log + 1; // length in bits
+        $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+        do {
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+            $rnd = $rnd & $filter; // discard irrelevant bits
+        } while ($rnd > $range);
+        return $min + $rnd;
+    }
+
+    public function getToken($length)
+    {
+        $token = "";
+        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+        
+        $max = strlen($codeAlphabet); // edited
+
+        for ($i=0; $i < $length; $i++) {
+            if($i>0){
+                $codeAlphabet.= "0123456789";
+                $max = strlen($codeAlphabet); 
+            }
+            $token .= $codeAlphabet[$this->crypto_rand_secure(0, $max-1)];
+        }
+
+        return $token;
+    }
+
     private function migrate(){
         if($this->func == "create"){
-            $fp=fopen(UTILITY_PATH.'/Migrations/filename.php','w');
-            fwrite($fp, 'data to be written');
+            $token = $this->getToken(32);
+            $fp=fopen(UTILITY_PATH."/Migrations/$token.php",'w');
+            $seed_template = "
+<?php
+use MightyCore\SEED;
+class $token extends SEED{
+    public function up(){
+
+    }
+
+    public function down(){
+
+    }
+}
+            ";
+            fwrite($fp, "$seed_template");
             fclose($fp);
         }
     }
---
+++ src/SEED.php
@@ -0,0 +1,10 @@
+<?php
+namespace MightyCore;
+class SEED {
+
+    public function plant(){
+        $this->up();
+        $this->down();
+    }
+
+}
