e5410696fd9773d8f05abdded6be162432d97792
Desmond <desmond@desmondchin.xyz>
2019-10-18 11:21:46 +0800

    change request param and cater CSRF

--- src/REQUEST.php
+++ src/REQUEST.php
@@ -6,6 +6,7 @@ class REQUEST{
     static $method;
     static $query;
     static $params;
+    static $csrfToken;
 
     static $ajax = false;
 
@@ -13,17 +14,33 @@ class REQUEST{
         REQUEST::$query = $query;
     }
 
+    public static function secure($request){
+        /**
+         * This is the first protection of the framework
+         * Filtering done here
+         */
+        foreach ((array) $request as $k => $v) {
+            $string = str_replace(' ', '-', $v); // Replaces all spaces with hyphens.
+            $request[$k] = preg_replace('/[^A-Za-z0-9\-\/]/', '', $string); // Removes special chars.
+
+            if($k == "csrf_token"){
+                REQUEST::$csrfToken = $request[k];
+            }
+        }
+        return $request;
+    }
+
     public static function init($request){
         $query = array();
         foreach ((array) $request as $k => $v) {
-            if ($k == 'request' && empty(ROUTE::$inbound)) {
+            if ($k == '_request_' && empty(ROUTE::$inbound)) {
                 $last_char = substr($v, -1);
                 if($last_char == "/"){
                     /**Remove appending slash for comparison */
                     $v = substr($v,0,strlen($v)-1);
                 }
                 ROUTE::$inbound = "/".$v;
-            }else if($k !== 'request'){
+            }else if($k !== '_request_'){
                 $query[$k] = $v;
             }
         }
