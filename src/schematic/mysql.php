40a0d7e0346f12aae18f0362a22ae5ba21810925
Desmond Chin <desmond.chin.cc93@gmail.com>
2019-12-08 18:12:05 +0800

    change schema to schematic

---
+++ src/schematic/mysql.php
@@ -0,0 +1,15 @@
+<?php
+namespace MightyCore;
+class SCHEMATIC{
+
+  private $table = null;
+  private $_main = '';
+
+  private function __construct($table) {
+    $this->table = $table;
+  }
+
+  public static function create($table, ){
+      return new SCHEMATIC($table);
+  }
+}
