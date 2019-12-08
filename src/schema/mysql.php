4bf664672238d6800faff07797c0beeed6b2f50b
Desmond Chin <desmond.chin.cc93@gmail.com>
2019-12-08 17:02:12 +0800

    add schema

---
+++ src/schema/mysql.php
@@ -0,0 +1,12 @@
+<?php
+namespace MightyCore;
+class SCHEMA{
+
+  private function setConnection(){
+
+  }
+  
+  public static function create($table, $func, $connection='default'){
+        
+  }
+}
