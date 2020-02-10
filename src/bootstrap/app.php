1e6e3b3e4aed969e93cc358e54d8e0ec8661cb6d
Desmond Chin <desmond.chin.cc93@gmail.com>
2020-02-10 21:03:29 +0800

    add env filtering

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -2,7 +2,15 @@
 
 function env($env, $default=false){
   if(getenv($env)){
-    return getenv($env);
+    $env = getenv($env);
+    if(preg_match('/^(["\']).*\1$/m', $env)){
+      $env = str_replace('"', '', $env);
+      $env = str_replace("'", '', $env);
+    }else{
+      $env = str_replace("\n", '', $env); // remove new lines
+      $env = str_replace("\r", '', $env);
+    }
+    return $env;
   }else{
     if($default){
       return $default;
