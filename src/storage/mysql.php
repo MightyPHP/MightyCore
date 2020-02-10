1d4aabc6a22a32a8c3689d22a8051808f9a25eb4
Desmond <desmond@desmondchin.xyz>
2020-02-10 16:02:09 +0800

    update storage to use env

--- src/storage/mysql.php
+++ src/storage/mysql.php
@@ -54,21 +54,16 @@ class STORAGE{
      * Init the DB object
      */
     public static function store($table, $db='default'){
-        $config = parse_ini_file(CONFIG_PATH."/databases.ini", true);
-        if (isset($config[$db])) {
-            $servername = $config[$db]['servername'];
-            $username = $config[$db]['username'];
-            $password = $config[$db]['password'];
-            $database = $config[$db]['database'];
-            try {
-                $db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return new STORAGE($db, $table);
-            } catch (PDOException $e) {
-                die($e);
-            }
-        } else {
-            throw new Exception('Database does not exist');
+        $servername = env('DB_'.strtoupper($db).'_HOST');
+        $username = env('DB_'.strtoupper($db).'_USERNAME');
+        $password = env('DB_'.strtoupper($db).'_PASSWORD');
+        $database = env('DB_'.strtoupper($db).'_DATABASE');
+        try {
+            $db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return new STORAGE($db, $table);
+        } catch (PDOException $e) {
+            die($e);
         }
     } 
 
