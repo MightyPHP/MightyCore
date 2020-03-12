ebb9ef7bf11aba32210030ec989acba92a930269
Desmond Chin <desmond.chin.cc93@gmail.com>
2020-03-12 22:44:45 +0800

    added global trans function

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * This part loads the .env values into putenv
+ * Critical to run first, else env() method will not return any value
+ */
 if(file_exists ( DOC_ROOT . ".env" )){
   $envFile = fopen(DOC_ROOT . ".env", "r");
   $contents = fread($envFile, filesize(DOC_ROOT . ".env"));
@@ -11,6 +15,38 @@ if(file_exists ( DOC_ROOT . ".env" )){
   fclose($envFile);
 }
 
+/**
+ * Global Translation function
+ * 
+ * @param string  $data   The file.key pair for translation look up
+ * @return  string
+ */
+function trans($data){
+  $data = explode(".", $data);
+  $file = $data[0];
+  $var = $data[1];
+
+  if(!empty($_SESSION['lang'])){
+      $mode = $_SESSION['lang'];
+  }else{
+      $mode = DEFAULT_LANG;
+  }
+
+  $lang = include(UTILITY_PATH."/Lang/$mode/$file.php");
+  if(!empty($lang[$var])){
+      return $lang[$var];
+  }else{
+      return '';
+  }
+}
+
+/**
+ * Grab ENV value
+ * 
+ * @param string  $env   The env key to obtain
+ * @param any $default  The default value to return if env not found
+ * @return  any
+ */
 function env($env, $default=false){
   if(getenv($env)){
     $env = getenv($env);
