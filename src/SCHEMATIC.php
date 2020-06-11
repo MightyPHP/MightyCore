b89105d8f1274a1567a5aa085bc9fb56c0885122
Desmond Chin <desmond.chin.cc93@gmail.com>
2020-06-11 14:33:05 +0800

    code clean up

--- src/SCHEMATIC.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include_once __DIR__.'/schematic/mysql.php';
--- src/VERSION.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace MightyCore;
-class VERSION{
-    public static function getVersion(){
-        return "v0.0.5";
-    }
-}
-?>
