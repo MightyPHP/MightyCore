0c2f82e7fbd0bbc86acc5a3d3a5fc0192e3a6b21
Desmond Chin <Desmond>
2021-04-24 18:16:54 +0800

    added database driver for session

--- src/Vault/SessionManager.php
+++ src/Vault/SessionManager.php
@@ -4,52 +4,100 @@ namespace MightyCore\Vault;
 
 class SessionManager
 {
-  public static function sessionStart($limit = 0, $path = '/', $domain = null, $secure = null)
+  private $db;
+
+  public function __construct()
+  {
+    $this->db = $this->getDefaultDB();
+  }
+
+  public function sessionStart($limit = 0, $path = '/', $domain = null, $secure = null)
   {
     $sessionDriver = config('session.driver');
 
     if($sessionDriver == "file"){
       session_save_path(DOC_ROOT."/Storage/Sessions");
-      // Set SSL level
-      $https = isset($secure) ? $secure : isset($_SERVER['HTTPS']);
-      
-      // Set session cookie options
-      session_set_cookie_params($limit, $path, $domain, $https, true);
-      session_start();
-
-      // Generate CSRF Token
-      self::generateCSRF();
-
-      // Make sure the session hasn't expired, and destroy it if it has
-      if (!self::validateSession()) {
-        $_SESSION = array();
-        session_destroy();
-        session_start();
-      }
     }else if($sessionDriver == "database"){
-      $session = self::getDatabaseSession();
+      session_set_save_handler(
+        array($this, "dbSessionOpen"),
+        array($this, "dbSessionClose"),
+        array($this, "dbSessionRead"),
+        array($this, "dbSessionWrite"),
+        array($this, "dbSessionDestroy"),
+        array($this, "dbSessionGc")
+      );
+    }
 
-      if(empty($session)){
-        $db = self::getDefaultDB();
+    // Set SSL level
+    $https = isset($secure) ? $secure : isset($_SERVER['HTTPS']);
+          
+    // Set session cookie options
+    // session_set_cookie_params($limit, $path, $domain, $https, true);
+    session_start();
 
-        $bytes = random_bytes(20);
-        $seshCookie = bin2hex($bytes);
+    // Generate CSRF Token
+    $this->generateCSRF();
 
-        $stmt = $db->prepare("INSERT INTO ".config('session.database.table')." (`id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES (?, ?, ?, ?, ?)");
-        $stmt->execute([$seshCookie, $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT'], json_encode(array()), gmdate('Y-m-d H:i:s')]);
-      }
+    // Make sure the session hasn't expired, and destroy it if it has
+    if (!$this->validateSession()) {
+      $_SESSION = array();
+      session_destroy();
+      session_start();
     }
   }
 
-  private static function getDatabaseSession(){
-    $db = self::getDefaultDB();
-    $seshCookie = $_COOKIE[config('session.cookie')];
+  public function dbSessionOpen(){
+    return true;
+  }
 
-    $stmt = $db->prepare("SELECT * FROM ".config('session.database.table')." WHERE id == ?");
-    $stmt->execute([$seshCookie]);
+  public function dbSessionClose(){
+    return true;
+  }
+
+  public function dbSessionRead($id){
+    $stmt = $this->db->prepare("SELECT * FROM ".config('session.database.table')." WHERE id = ?");
+    $stmt->execute([$id]);
     $data = $stmt->fetch(\PDO::FETCH_OBJ);
 
-    return json_decode($data->payload, true);
+    if($data){
+      return $data->payload;
+    }else{
+      return "";
+    }
+  }
+
+  public function dbSessionWrite($id ,$data){
+    $stmt = $this->db->prepare("SELECT * FROM ".config('session.database.table')." WHERE id = ?");
+    $stmt->execute([$id]);
+    $session = $stmt->fetch(\PDO::FETCH_OBJ);
+
+    if($session == null){
+      $stmt = $this->db->prepare("INSERT INTO ".config('session.database.table')." (`id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES (?,?,?,?,?)");
+      $stmt->execute([$id, $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT'], $data, gmdate('Y-m-d H:i:s')]);
+    }else{
+      $stmt = $this->db->prepare("UPDATE ".config('session.database.table')." SET payload=?, last_activity=? WHERE id = ?");
+      $stmt->execute([$data, gmdate('Y-m-d H:i:s'), $id]);
+    }
+
+    return true;
+  }
+
+  public function dbSessionDestroy($id){
+    $stmt = $this->db->prepare("DELETE FROM ".config('session.database.table')." WHERE id = ?");
+    $stmt->execute([$id]);
+    $data = $stmt->fetch(\PDO::FETCH_OBJ);
+
+    return true;
+  }
+
+  public function dbSessionGc($max){
+    $old = time() - $max;
+
+    $stmt = $this->db->prepare("DELETE FROM ".config('session.database.table')." WHERE last_activity < ?");
+    $stmt->execute([gmdate("Y-m-d H:i:s", $old)]);
+    $data = $stmt->fetch(\PDO::FETCH_OBJ);
+
+    return true;
   }
 
   private static function getDefaultDB()
@@ -98,7 +146,7 @@ class SessionManager
       $db = self::getDefaultDB();
       $seshCookie = $_COOKIE[config('session.cookie')];
 
-      $stmt = $db->prepare("SELECT * FROM ".config('session.database.table')." WHERE id == ?");
+      $stmt = $db->prepare("SELECT * FROM ".config('session.database.table')." WHERE id = ?");
       $stmt->execute([$seshCookie]);
       $data = $stmt->fetch(\PDO::FETCH_OBJ);
 
@@ -107,7 +155,7 @@ class SessionManager
       }
 
       if($value != null){
-        $stmt = $db->prepare("SELECT * FROM ".config('session.database.table')." WHERE id == ?");
+        $stmt = $db->prepare("SELECT * FROM ".config('session.database.table')." WHERE id = ?");
         $stmt->execute([$seshCookie, $value]);
       }else{
         $session = json_decode($data->payload, true);
@@ -116,14 +164,17 @@ class SessionManager
     }
   }
 
-  static protected function generateCSRF()
+  protected function generateCSRF()
   {
-      if (self::sessionSetterGetter('csrf_token') === null) {
-        self::sessionSetterGetter('csrf_token', bin2hex(random_bytes(32)));
-      }
+    if (!isset($_SESSION['csrf_token']) || $_SESSION['csrf_token'] === null) {
+      $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+      // if (self::sessionSetterGetter('csrf_token') === null) {
+      //   self::sessionSetterGetter('csrf_token', bin2hex(random_bytes(32)));
+      // }
   }
 
-  static protected function preventHijacking()
+  protected function preventHijacking()
   {
     if (!isset($_SESSION['IPaddress']) || !isset($_SESSION['userAgent']))
       return false;
@@ -137,7 +188,7 @@ class SessionManager
     return true;
   }
 
-  static function regenerateSession()
+  public static function regenerateSession()
   {
     // If this session is obsolete it means there already is a new id
     if (isset($_SESSION['OBSOLETE']) && $_SESSION['OBSOLETE'] == true)
@@ -163,7 +214,7 @@ class SessionManager
     unset($_SESSION['EXPIRES']);
   }
 
-  static protected function validateSession()
+  protected function validateSession()
   {
     if (isset($_SESSION['OBSOLETE']) && !isset($_SESSION['EXPIRES']))
       return false;
